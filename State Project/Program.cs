75705cd627ed0f2bef6eb5eb35714912700a8b9a
Ryan Pannell <rap23641@email.vccs.edu>
2025-02-11 17:44:11 -0500

    Assignment Completed

--- State Project/Program.cs
+++ State Project/Program.cs
@@ -1,10 +1,50 @@
 ﻿namespace State_Project
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //List portion
+            State newState = new State("CA", "California", "Sacramento", 39538223);
+            List<State> stateList = State.StateList;
+            stateList.Add(newState);
+            Console.WriteLine("States in the list:");
+            foreach (var state in stateList)
+            {
+                Console.WriteLine($"Code: {state.Code}, Name: {state.Name}, Capitol: {state.Capitol}, Population: {state.Population}");
+            }
+            Console.WriteLine("");
+            stateList.Remove(newState);
+
+            //State dictionary portion
+            SortedDictionary<string, string> statesDictionary = State.StatesDictionary;
+            statesDictionary[newState.Code] = newState.Name;
+            Console.WriteLine("States in the dictionary:");
+            foreach (var kvp in statesDictionary)
+            {
+                Console.WriteLine($"Code: {kvp.Key}, Name: {kvp.Value}");
+            }
+            Console.WriteLine("");
+            statesDictionary.Remove(newState.Code);
+
+            //Sorted list portion
+            SortedList<string, State> sortedStates = State.SortedStates;
+            sortedStates[newState.Code] = newState;
+            Console.WriteLine("States in the sorted list:");
+            foreach (var kvp in sortedStates)
+            { 
+                Console.WriteLine($"Code: {kvp.Key}, Name: {kvp.Value.Name}, Capitol: {kvp.Value.Capitol}, Population: {kvp.Value.Population}");
+            }
+            Console.WriteLine("");
+            statesDictionary.Remove(newState.Code);
+
+            //State populations portion
+            List<int> statePops = State.StatePops;
+            statePops.Add(newState.Population);
+            int totalPopulation = statePops.Sum();
+            Console.WriteLine($"Total population of all states: {totalPopulation}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ State Project/State.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_Project
+{
+   
+    #region States
+    public class State
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Capitol { get; set; }
+        public int Population { get; set; }
+        public State(string code, string name, string capitol, int population)
+        {
+            Code = code;
+            Name = name;
+            Capitol = capitol;
+            Population = population;
+        }
+        private static State[] states = {
+            new State("VA", "Virginia", "Richmond", 8535519),
+            new State("AL", "Alabama", "Montgomery", 4903185),
+            new State("MS", "Mississippi", "Jackson", 2976149),
+            new State("IL", "Illinois", "Springfield", 12671821),
+            new State("TX", "Texas", "Austin", 28995881),
+            new State("NC", "North Carolina", "Raleigh", 10488084),
+            new State("UT", "Utah", "Salt Lake City", 3205958),
+            new State("PA", "Pennsylvania", "Harrisburg", 12801989)
+        };
+        public static State[] States
+        {
+            get
+            {
+                return states;
+            }
+        }
+        /// <summary>
+        /// Return a Dictionary of State Names index by the State Code
+        /// </summary>
+        public static List<State> StateList
+        {
+            get
+            {
+                return new List<State>(states);
+            }
+        }
+        public static SortedDictionary<string, string> StatesDictionary
+        {
+            get
+            {
+                var dictionary = new SortedDictionary<string, string>();
+                foreach (var state in states)
+                {
+                    dictionary[state.Code] = state.Name;
+                }
+                return dictionary;
+            }
+        }
+        public static SortedList<string, State> SortedStates
+        {
+            get
+            {
+                var sortedList = new SortedList<string, State>();
+                foreach (var state in states)
+                {
+                    sortedList[state.Code] = state;
+                }
+                return sortedList;
+            }
+        }
+        public static List<int> StatePops
+        {
+            get
+            {
+                var populations = new List<int>();
+                foreach (var state in states)
+                { 
+                    populations.Add(state.Population);
+                }
+                return populations;
+            }
+        }
+        public static int[] Populations
+        {
+            get
+            {
+                int len = states.Length;
+                int[] pops = new int[len];
+                for (int i = 0; i < len; i++)
+                {
+                    pops[i] = states[i].Population;
+                }
+                return pops;
+            }
+        }
+    }
+    #endregion
+}
